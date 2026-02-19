b69f896f8c73973bb716e794c0e06294f487a329
Naf'an Rizkilah <100104632+riizlaah@users.noreply.github.com>
2026-02-19 11:28:51 +0700

    small progress

--- app/src/main/java/nr/dev/bruv/MainActivity.kt
+++ app/src/main/java/nr/dev/bruv/MainActivity.kt
@@ -5,20 +5,25 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -27,6 +32,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -60,25 +66,41 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LoginScreen(modifier: Modifier, onLogin: () -> Unit, onRegist: () -> Unit) {
-    var username by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     Column(modifier.fillMaxSize().padding(36.dp), verticalArrangement = Arrangement.spacedBy(8.dp)) {
         Image(painter = painterResource(id = R.drawable.esemka), contentDescription = "Esemka Gym Logo",
             modifier = Modifier.padding(horizontal = 30.dp).fillMaxWidth())
         Text("Sign In", fontSize = 4.em, fontWeight = FontWeight.Bold)
         Text("Hi there! Build your muscle with us!", color = Color.Gray)
-        OutlinedTextField(modifier = Modifier.fillMaxWidth(), value = username, onValueChange = {str: String -> username = str}, label = {Text("Username")})
-        OutlinedTextField(modifier = Modifier.fillMaxWidth(), value = password, onValueChange = {str: String -> password = str}, label = {Text("Password")})
+        Column {
+            BoldOrangeText("Email")
+            TextField(
+                modifier = Modifier.fillMaxWidth().borderBottom(),
+                value = email, onValueChange = {str: String -> email = str},
+                placeholder = {Text("Email")},
+                colors = resetTextField()
+            )
+        }
+        Column {
+            BoldOrangeText("Password")
+            TextField(
+                modifier = Modifier.fillMaxWidth().borderBottom(),
+                value = password, onValueChange = {str: String -> password = str},
+                placeholder = {Text("Password")},
+                colors = resetTextField()
+            )
+        }
         Button(
             onClick = onLogin,
             shape = RoundedCornerShape(8.dp),
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth(),
         ) {
             Text("Sign In", fontWeight = FontWeight.Bold)
         }
         CenterCol {
             Text("or", fontWeight = FontWeight.Bold, color = Color.Gray)
-            TextButton(onClick = onRegist) {
+            TextButton(onClick = onRegist, contentPadding = PaddingValues(0.dp)) {
                 Text("Sign Up", fontWeight = FontWeight.Bold)
             }
         }
@@ -87,15 +109,29 @@ fun LoginScreen(modifier: Modifier, onLogin: () -> Unit, onRegist: () -> Unit) {
 
 @Composable
 fun RegisterScreen(modifier: Modifier, onLogin: () -> Unit) {
-    var username by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
     var isMale by remember { mutableStateOf(true) }
     Column(modifier.fillMaxSize().padding(38.dp), verticalArrangement = Arrangement.spacedBy(8.dp)) {
         Text("Sign Up", fontSize = 4.em, fontWeight = FontWeight.Bold)
         Text("Hi there! Register your gym member!", color = Color.Gray)
-        OutlinedTextField(modifier = Modifier.fillMaxWidth(), value = username, onValueChange = {str: String -> username = str}, label = {Text("Username")})
-        OutlinedTextField(modifier = Modifier.fillMaxWidth(), value = password, onValueChange = {str: String -> password = str}, label = {Text("Password")})
-        Text("Gender")
+        Column {
+            BoldOrangeText("Email")
+            TextField(
+                modifier = Modifier.fillMaxWidth().borderBottom(), value = email,
+                onValueChange = {str: String -> email = str}, placeholder = {Text("Password")},
+                colors = resetTextField()
+            )
+        }
+        Column {
+            BoldOrangeText("Password")
+            TextField(
+                modifier = Modifier.fillMaxWidth().background(Color.Transparent).borderBottom(), value = password,
+                onValueChange = {str: String -> password = str},
+                colors = resetTextField()
+            )
+        }
+        BoldOrangeText("Gender")
         Column {
             CenterRow(Arrangement.Start) {
                 RadioButton(isMale, onClick = {isMale = true})
@@ -131,7 +167,7 @@ fun HomeScreen(modifier: Modifier) {
 
 @Composable
 fun CenterCol(function: @Composable () -> Unit) {
-    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.spacedBy(8.dp)) {
+    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
         function()
     }
 }
@@ -143,8 +179,31 @@ fun CenterRow(arrangement: Arrangement.Horizontal = Arrangement.Center, function
     }
 }
 
-//fun Modifier.borderBottom(color: Color = Color.Gray, strokeWidth: Dp = 2.dp): Modifier =
-//    this.drawBehind {
-//        val y = size.height - strokeWidth.toPx() / 2
-//
-//    }
+@Composable
+fun BoldOrangeText(text: String) {
+    Text(text, color = MaterialTheme.colorScheme.primary, fontWeight = FontWeight.Bold)
+}
+
+fun Modifier.borderBottom(color: Color = Color.LightGray, strokeWidth: Dp = 2.dp): Modifier =
+    this.drawBehind {
+        val y = size.height - strokeWidth.toPx() / 2
+        drawLine(
+            color,
+            Offset(0f, y),
+            Offset(size.width, y),
+            strokeWidth.toPx()
+        )
+    }
+
+@Composable
+fun resetTextField(): TextFieldColors {
+    return TextFieldDefaults.colors(
+        focusedIndicatorColor = Color.Transparent,
+        unfocusedIndicatorColor = Color.Transparent,
+        disabledTextColor = Color.Transparent,
+        errorIndicatorColor = Color.Transparent,
+        focusedContainerColor = Color.Transparent,
+        unfocusedContainerColor = Color.Transparent,
+        disabledContainerColor = Color.Transparent,
+    )
+}
